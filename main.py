d02930479685b12c6f5ca0452b01c55f92933114
Michael Parque <michael.t.parque@outlook.com>
2023-08-14 02:49:17 -0400

    finish implementing obs_move_file without dry run

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import subprocess
 from typing import List
 
 
+VAULT_NAME = "notes-personal-2"
 VAULT_PATH = "/Users/mparq/Documents/notes-personal-2"
 CURRENT_GREP_PATTERN = "daily_notes"
+IS_DRY_RUN = True
 
 
 def main():
     files_to_rename = list_notes()
     to_rename_list = note_str_to_list(files_to_rename)
-    obs_move_files(to_rename_list)
+    obs_move_files(to_rename_list, dry_run=IS_DRY_RUN, obs_vault_name=VAULT_NAME)
 
 
 def list_notes():
@@ -28,12 +30,14 @@ def note_str_to_list(notes_str: str) -> List[str]:
     return [line for line in notes_str.split('\n') if line.strip() != '']
 
 
-def obs_move_files(to_move_list: List[str], dry_run=True):
+def obs_move_files(to_move_list: List[str], dry_run=True, obs_vault_name=None):
+    assert obs_vault_name is not None
     # hardcoded file move logic
-    for to_move in to_move_list:
+    for i, to_move in enumerate(to_move_list):
         new_name = update_filename(to_move)
-        if dry_run:
-            print(f"DRY mv {to_move} {new_name}")
+        print(f"[{i+1}/{len(to_move_list)}] {'DRY' if dry_run else ''} obs move {to_move} {new_name} --vault \"{obs_vault_name}\"")
+        if not dry_run:
+            subprocess.run(["obs", "move", to_move, new_name, "--vault", obs_vault_name])
 
 
 def update_filename(to_move: str):

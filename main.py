5837c1f6a4fcd299c0c4ebf9e0751097f0666ee5
Michael Parque <michael.t.parque@outlook.com>
2023-08-14 02:38:25 -0400

    impl dry run of obs_move_files with hardcoded rename logic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import subprocess
+from typing import List
 
 
 VAULT_PATH = "/Users/mparq/Documents/notes-personal-2"
@@ -7,7 +8,8 @@ CURRENT_GREP_PATTERN = "daily_notes"
 
 def main():
     files_to_rename = list_notes()
-    print(files_to_rename)
+    to_rename_list = note_str_to_list(files_to_rename)
+    obs_move_files(to_rename_list)
 
 
 def list_notes():
@@ -20,6 +22,26 @@ def list_notes():
     ls_call.stdout.close()
     out, err = grep_call.communicate()
     return out.decode()
+
+
+def note_str_to_list(notes_str: str) -> List[str]:
+    return [line for line in notes_str.split('\n') if line.strip() != '']
+
+
+def obs_move_files(to_move_list: List[str], dry_run=True):
+    # hardcoded file move logic
+    for to_move in to_move_list:
+        new_name = update_filename(to_move)
+        if dry_run:
+            print(f"DRY mv {to_move} {new_name}")
+
+
+def update_filename(to_move: str):
+    # assumes to_move=YYYYMMDD....md
+    year = to_move[:4]
+    month = to_move[4:6]
+    date = to_move[6:8]
+    return f"{year}-{month}-{date}.md"
 
 
 if __name__ == "__main__":
